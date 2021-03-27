78fe6aba1c3b9908d87caa7edd7e57cb4763b37e
znerol <lo+github@znerol.ch>
2021-03-27 12:22:21 +0100

    chore(docs): More detailed docs on demo page and in the readme

--- packages/demo/demo.js
+++ packages/demo/demo.js
@@ -1,18 +1,24 @@
 import { Lookup } from "@chzip/lookup";
 
-const chzip = new Lookup("data");
+const chzip = new Lookup();
 
 addEventListener('DOMContentLoaded', () => {
   const zip = document.getElementById('zip');
   const city = document.getElementById('city');
   const canton = document.getElementById('canton');
   const hits = document.getElementById('hits');
+  const reset = document.getElementById('reset');
+
+  const clear = () => {
+    hits.innerHTML = '';
+    hits.classList.remove('hits-visible');
+  };
 
   const choose = (record) => {
-    hits.innerHTML = '';
     zip.value = record.zip;
     city.value = record.cty;
     canton.value = record.reg;
+    clear();
   };
 
   const show = (result) => {
@@ -23,6 +29,12 @@ addEventListener('DOMContentLoaded', () => {
       item.addEventListener('click', () => choose(record));
       return item;
     }));
+    if (result.length > 0) {
+        hits.classList.add('hits-visible');
+    }
+    else {
+        hits.classList.remove('hits-visible');
+    }
   };
 
   zip.addEventListener('input', async () => {
@@ -33,5 +45,12 @@ addEventListener('DOMContentLoaded', () => {
   city.addEventListener('input', async () => {
     const result = await chzip.lookup('cty', city.value);
     show(result);
+  });
+
+  reset.addEventListener('click', () => {
+      zip.value = '';
+      city.value = '';
+      canton.value = '';
+      clear();
   });
 });
